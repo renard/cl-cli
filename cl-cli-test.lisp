dfa59da6953130b20dac851b6c1aa152c7efb241
Sébastien Gross <sebchezwamorg>
2014-06-11 20:01:29 +0200

    Update test

--- cl-cli-test.lisp
+++ cl-cli-test.lisp
@@ -1,36 +1,34 @@
 (in-package #:cl-cli)
 
+(defvar *debug*)
+(defvar *dir*)
 (defparameter *options*
-  '((debug nil "Run in debug mode" :alias ("-d"))
-    (dir "/tmp" "Change to directory" :metavars ("DIR"))))
+  '((*debug* nil "Run in debug mode" :alias ("-d"))
+    (*dir* "/tmp" "Change to directory" :metavars ("DIR"))))
 
 (defparameter *commands*
-  '((server-start
-     ("server" "start")
-     (&optional
-      (restart "restart instead of start")
-      &key (delay 2 "Second to wait" :metavars ("DELAY")))
-     "Start or restart server"
-     (progn
-       (when *debug*
-	 (format t "Delay: ~a Restart: ~a~%" delay restart))
-       (format t "Server ~a in ~a~%"
-	       (if restart "restarted" "started") *dir*)))
+  (list
+   (cl-cli::defcommand
+       ("server" "start")
+       ((restart nil "restart instead of start")
+	(delay 2 "Second to wait" :metavars ("DELAY")))
+       "Start or restart server"
+     (when *debug*
+       (format t "Delay: ~a Restart: ~a~%" delay restart))
+     (format t "Server ~a in ~a~%"
+	     (if restart "restarted" "started") *dir*)
+     "start")
 
-    (server-stop
-     ("server" "stop")
-     (&key (delay 2 "Second to wait" :metavars ("DELAY")))
-     "Stop server"
-     (progn
-       (when *debug*
-	 (format t "Delay: ~a~%" delay))
-       (format t "Server stopped in ~a~%" *dir*)))))
+   (cl-cli::defcommand
+       ("server" "stop")
+       ((delay 2 "Second to wait" :metavars ("DELAY")))
+       "Stop server"
+     (when *debug*
+       (format t "Delay: ~a~%" delay))
+     (format t "Server stopped in ~a~%" *dir*)
+     "stop")))
 
-(defparameter *cli*
-  '("./tool" "--dir" "/path/to/chroot" "--debug"
-    "server" "start" "--restart" "--delay" "3"
-    "instance1" "isntance2"))
-
-
-;; call:
-;; (cl-cli::run-command cl-cli::*cli* cl-cli::*options* cl-cli::*commands*)
+;; (cl-cli::run-command
+;;  '("./tool" "--dir" "/path/to/chroot" "--debug"
+;;    "server" "start" "--restart" "--delay" "3"
+;;    "instance1" "isntance2") *options* *commands*)
